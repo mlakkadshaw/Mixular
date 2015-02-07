83137ff0ae49684f3fe31427f6c888092c872ee9
Mohammed Lakkadshaw <mohammed.lakkadshaw@gmail.com>
2015-02-07 10:41:56 +0530

    inital commit

---
+++ Readme.md
@@ -0,0 +1,49 @@
+# Mixular: Mixpanel Tracking for AngularJS
+
+Mixular is directive which you can use in your angularJS applications to track mix panel events.
+
+## How to use it
+
+You must include the Mixpanel javascript tracking library, then add the tracking-directive dependency in your AngularJS app.
+
+	var app = angular.module("yourApp", ["mixular-directive"]);
+	
+After that, you can start tracking events on your pages, like this:
+
+	<a href="#/feedback" mixular="click" eventLabel="Feedback Clicked" propUser="James">Feedback</a>	
+	
+	
+You will have to add `mixular` and specify the event on which you want to do the tracking. 
+All the standard event types are supported:
+
++ click
++ focus
+3. hover
+4. keydown
+5. keypress
+6. keyup
+7. mousedown
+8. mouseenter
+9. mouseleave
+10. mouseout
+11. mouseover
+12. mouseup
+13. scroll
+14. select
+
+Using `eventLabel` property you can specify the label of the event e.g "Banner Ad clicked".
+
+### Passing properties
+
+To pass multiple properties for the event you can type the keyword prop followed by the property name e.g propProduct="Laptop" propPrice="500"
+
+
+## Built by [ML&Co](http://mohammedlakkadshaw.com)
+
+We are a mobile and web-app development company, to learn more about us, visit our website or blog:
+
+Website: [http://mohammedlakkadshaw.com](http://mohammedlakkadshaw.com)
+
+Blog: [http://blog.mohammedlakkadshaw.com](http://blog.mohammedlakkadshaw.com)
+
+
---
+++ dist/angular-mixular-directive.js
@@ -0,0 +1,41 @@
+angular.
+/**
+ * mixular-directive Module
+ *
+ * Description
+ */
+angular.module('mixular-directive', []).directive('', ['',
+    function() {
+        // Runs during compile
+        return {
+            // name: '',
+            // priority: 1,
+            // terminal: true,
+            // scope: {}, // {} = isolate, true = child, false/undefined = no change
+            // controller: function($scope, $element, $attrs, $transclude) {},
+            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+            // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+            // template: '',
+            // templateUrl: '',
+            // replace: true,
+            // transclude: true,
+            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+            link: function($scope, iElm, iAttrs, controller) {
+                elm.on(attrs.mixular, function(evnt) {
+                    var properties = {};
+
+                    //Add all the attributes that begin with
+                    //prop in the properties object.
+                    for (key in attrs) {
+                        if (key.indexOf("prop")) {
+                            properties[key] = attrs[key];
+                        }
+                    }
+
+                    //Send the tracking info to mix-panel
+                    mixpanel.track(attrs.eventLabel, properties);
+                });
+            }
+        };
+    }
+]);
